9c2a73b711e79e32ee2d3b6890530c2cdb3df49a
CappelleJess <cappellejessica.lcj@gmail.com>
2022-01-31 14:56:00 +0100

    load images
    FAILED

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 window.onload = function ()
 		{
 			//BACKHAIR
-			var charabackhair = new Image();
-			var charabackhairnum = Math.floor(Math.random()*1)+1;
-			var charabackhairname = "backhair" + charabackhairnum + ".png";
-			charabackhair.src = charabackhairname;
+			var charabackhair = ["backhair1.png", "backhair2.png", "backhair3.png", "backhair4.png"];
 
 			//BODY
 			var charabody = new Image();
@@ -49,10 +46,13 @@ window.onload = function ()
 			charafronthair.src = charafronthairname;
 
 			//BACKHAIR LOAD
-			charabackhair.onload = function()
-			{
-				buildchara();
-			}
+			function getcharabackhair(imgAr, path) {
+    		path = path || 'items/backhair';
+    		var num = Math.floor( Math.random() * imgAr.length );
+    		var img = imgAr[ num ];
+    		var imgStr = '<img src=' + path + img + '" alt = "">';
+    		document.write(imgStr); document.close();
+}
 
 			//BODY LOAD
 			charabody.onload = function()
@@ -100,8 +100,8 @@ window.onload = function ()
 			{
 				var canvas = document.getElementById('canvas');
 				var ctx = canvas.getContext('2');
-				canvas.width = 	400;
-				canvas.height = 400;
+				canvas.width = 	500;
+				canvas.height = 500;
 
 				//BACKGROUND COLOUR
 				var r = Math.floor(Math.random() * (255 - 100 - 1) + 100);
@@ -109,24 +109,24 @@ window.onload = function ()
 				var b = Math.floor(Math.random() * (255 - 100 - 1) + 100);
 				var bgcol = "#" + r.toString(16) + g.toString(16) + b.toString(16);
 				ctx.fillStyle = bgcol;
-				ctx.fillRect (0, 0, 400, 400);
+				ctx.fillRect (0, 0, 500, 500);
 
-				//DRAW BACKHAIR
-				ctx.drawImage(charabackhair,((400-charabackhair.width)/2),50);
-				//DRAW BODY
-				ctx.drawImage(charabody,((400-charabody.width)/2),50);
-				//DRAW FACE
-				ctx.drawImage(charahead,((400-charahead.width)/2),50);
-				//DRAW EYES
-				ctx.drawImage(charaeyes,((400-charaeyes.width)/2),50);
-				//DRAW EYEBROWS
-				ctx.drawImage(charaeyebrows,((400-charaeyebrows.width)/2),50);
-				//DRAW MOUTH
-				ctx.drawImage(charamouth,((400-charamouth.width)/2),50);
-				//DRAW NOSE
-				ctx.drawImage(charanose,((400-charanose.width)/2),50);
-				//DRAW FRONTHAIR
-				ctx.drawImage(charafronthair,((400-charafronthair.width)/2),50);
+				// //DRAW BACKHAIR
+				// ctx.drawImage(charabackhair,((500-charabackhair.width)/2),50);
+				// //DRAW BODY
+				// ctx.drawImage(charabody,((500-charabody.width)/2),50);
+				// //DRAW FACE
+				// ctx.drawImage(charahead,((500-charahead.width)/2),50);
+				// //DRAW EYES
+				// ctx.drawImage(charaeyes,((500-charaeyes.width)/2),50);
+				// //DRAW EYEBROWS
+				// ctx.drawImage(charaeyebrows,((500-charaeyebrows.width)/2),50);
+				// //DRAW MOUTH
+				// ctx.drawImage(charamouth,((500-charamouth.width)/2),50);
+				// //DRAW NOSE
+				// ctx.drawImage(charanose,((500-charanose.width)/2),50);
+				// //DRAW FRONTHAIR
+				// ctx.drawImage(charafronthair,((500-charafronthair.width)/2),50);
 
 			}
 		}
